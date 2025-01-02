d8a4b7709cb78215247906bd5d43e076ef39125b
JJ <jeremyjacob101@gmail.com>
2025-01-02 14:54:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-section">
+        <h3>jjfootball</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
